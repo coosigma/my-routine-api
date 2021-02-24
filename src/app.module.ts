1de18de3ab35a1f3692f05a743796316e0507216
BL <iceliang@gmail.com>
2021-02-25 12:16:01 +1300

    Installed GraphQL modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
